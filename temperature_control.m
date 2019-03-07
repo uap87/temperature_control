d46cef940a7a6092d976cbb15d8d6df8a6651f21
uap87 <pateludit87@gmail.com>
2019-03-06 19:04:11 -0500

    final commit

--- temperature_control.m
+++ temperature_control.m
@@ -1,15 +1,15 @@
-% Orbit 1: Control
-G=6.673889E-11; % m^3 kg^-1 s^-2
-Ms=1.9891E30; % kg
-Rs=696000000; % m
-tempS=5800;
-x=149597870000; % m regular
-y=0; % m
-U=0; % m/s
-V=29788; % m/s regular
-tmax=1; % s
-clockmax=317; % steps
-dt=100000/tmax;
+% % Orbit 1: Control
+% G=6.673889E-11; % m^3 kg^-1 s^-2
+% Ms=1.9891E30; % kg
+% Rs=696000000; % m
+% tempS=5800;
+% x=149597870000; % m regular
+% y=0; % m
+% U=0; % m/s
+% V=29788; % m/s regular
+% tmax=1; % s
+% clockmax=317; % steps
+% dt=100000/tmax;
 
 % % Orbit 2: Velocity Increases
 % G=6.673889E-11; % m^3 kg^-1 s^-2
@@ -19,12 +19,12 @@ dt=100000/tmax;
 % x=149597870000; % m regular
 % y=0; % m
 % U=0; % m/s
-% V=29788*1.2; % m/s regular
+% V=29788*1.1; % m/s regular
 % tmax=1; % s
-% clockmax=755; % steps
+% clockmax=451; % steps
 % dt=100000/tmax;
 
-% % Orbit 3: Velocity Decreases
+% % Orbit IGNORE: Velocity Increases
 % G=6.673889E-11; % m^3 kg^-1 s^-2
 % Ms=1.9891E30; % kg
 % Rs=696000000; % m
@@ -32,10 +32,23 @@ dt=100000/tmax;
 % x=149597870000; % m regular
 % y=0; % m
 % U=0; % m/s
-% V=29788*0.8; % m/s regular
+% V=29788*1.05; % m/s regular
 % tmax=1; % s
-% clockmax=201; % steps
+% clockmax=400; % steps
 % dt=100000/tmax;
+
+% Orbit 3: Velocity Decreases
+G=6.673889E-11; % m^3 kg^-1 s^-2
+Ms=1.9891E30; % kg
+Rs=696000000; % m
+tempS=5800;
+x=149597870000; % m regular
+y=0; % m
+U=0; % m/s
+V=29788*0.9; % m/s regular
+tmax=1; % s
+clockmax = 245; % steps
+dt=100000/tmax;
 
 % Energy stays Constant Half
 % % Orbit 4: * 1/6
@@ -109,6 +122,7 @@ xsave=zeros(1,clockmax);
 ysave=zeros(1,clockmax);
 tsave=zeros(1,clockmax);
 tempEsave = zeros(1,clockmax);
+total_temp=0;
 for clock=1:clockmax
 %     if y<0
 %         clock
@@ -119,6 +133,19 @@ for clock=1:clockmax
     x=x+dt*U;
     y=y+dt*V;
     tempE = tempS*sqrt(Rs/(2*R));
+    tempE = ((tempE - 273.15) * 9/5) + 32;
+    total_temp = total_temp+tempE;
+    if clock == 1
+        min_temp = tempE;
+        max_temp = tempE;
+    else
+        if tempE < min_temp
+            min_temp = tempE;
+        end
+        if tempE > max_temp
+            max_temp = tempE;
+        end
+    end
     xsave(clock)=x;
     ysave(clock)=y;
     tsave(clock)=clock*dt;
@@ -131,41 +158,6 @@ for clock=1:clockmax
     plot(tsave,tempEsave)
     drawnow
 end
-total_temp=0;
-min_temp = tempEsave(1);
-max_temp = tempEsave(1);
-for counter = 1:clockmax
-    total_temp = total_temp+tempEsave(counter);
-    if tempEsave(counter) < min_temp
-        min_temp = tempEsave(counter);
-    end
-    if tempEsave(counter) > max_temp
-        max_temp = tempEsave(counter);
-    end
-end
-% total_entries = size(tempEsave);
 average_temp = total_temp/clockmax
 min_temp
 max_temp
-% G=1; % m^3 kg^-1 s^-2
-% Ms=2; % kg
-% x=1; % m
-% y=0; % m
-% U=0; % m/s
-% V=sqrt(2); % m/s
-% tmax=50000; % s
-% clockmax=1000; % steps
-% dt=clockmax/tmax;
-% xsave=zeros(1,clockmax);
-% ysave=zeros(1,clockmax);
-% for clock=1:clockmax
-%     R=sqrt(x^2+y^2);
-%     U=U-dt*G*Ms*x/R^3;
-%     V=V-dt*G*Ms*y/R^3;
-%     x=x+dt*U;
-%     y=y+dt*V;
-%     xsave(clock)=x;
-%     ysave(clock)=y;
-%     plot(0,0,'r*',xsave,ysave)
-%     drawnow
-% end
